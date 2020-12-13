1a434cf6203c9e313c17005524f4210f61d57e3e
Batyrevsssemyon <55916069+Batyrevsssemyon@users.noreply.github.com>
2020-12-13 12:13:32 +0700

    Add files via upload

--- 25.py
+++ 25.py
@@ -1,13 +1,8 @@
-A = 185311
-B = 185330
-count = 2
-k = []
-for i in range (A, B + 1):
+for i in range (185311, 185331):
+    k = [1]
     for j in range (2, i // 2 + 1):
         if i % j == 0:
-            count += 1
             k.append(j)
-    if count == 4:
-        print(1, *k, i)
-    k = []
-    count = 2
+    k.append(i)
+    if len(k) == 4:
+        print(*k)
